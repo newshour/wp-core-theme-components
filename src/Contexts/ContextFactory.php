4b237b2196bae6f4b9f856465adcd3a693d0b231
Stephan Rohde <srohde@newshour.org>
2021-10-26 11:17:23 -0400

    Initial commit.

---
+++ src/Contexts/ContextFactory.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Factory class for creating Context objects.
+ */
+
+namespace NewsHour\WPCoreThemeComponents\Contexts;
+
+use Timber\Timber;
+
+use NewsHour\WPCoreThemeComponents\Http\Factories\RequestFactory;
+
+/**
+ * Factory class for creating Context objects.
+ */
+class ContextFactory {
+
+    /**
+     * Returns the default context object depending on WP's "template type". If
+     * an AJAX request is detected, AjaxContext is returned.
+     *
+     * - single-*.php > PostContext
+     * - front-page.php > PageContext
+     * - page.php > PageContext
+     *
+     * etc...
+     *
+     * Defaults to PageContext.
+     *
+     * @return Context
+     */
+    public static function default(): Context {
+
+        if (is_single()) {
+            return self::post();
+        }
+
+        if (is_home() || is_front_page() || is_page()) {
+            return self::page();
+        }
+
+        if (wp_doing_ajax() || RequestFactory::get()->isXmlHttpRequest()) {
+            return self::ajax();
+        }
+
+        return self::page();
+
+    }
+
+    /**
+     * Returns a "page" context object.
+     *
+     * @return Context
+     */
+    public static function page(): Context {
+
+        return new PageContext(RequestFactory::get(), Timber::context());
+
+    }
+
+    /**
+     * Returns a "post" context object.
+     *
+     * @return Context
+     */
+    public static function post(): Context {
+
+        return new PostContext(RequestFactory::get(), Timber::context());
+
+    }
+
+    /**
+     * Returns an AJAX context object.
+     *
+     * @return Context
+     */
+    public static function ajax(): Context {
+
+        return new AjaxContext(RequestFactory::get());
+
+    }
+
+}
