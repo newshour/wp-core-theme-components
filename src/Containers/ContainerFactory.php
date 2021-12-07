22c4e0695fe5d5d7640110331d0156ead4937a7a
Stephan Rohde <srohde@newshour.org>
2021-12-07 15:36:11 -0500

    Adding bundles, parameters.

--- src/Containers/ContainerFactory.php
+++ src/Containers/ContainerFactory.php
@@ -9,20 +9,31 @@ namespace NewsHour\WPCoreThemeComponents\Containers;
 use Exception;
 use InvalidArgumentException;
 use Composer\Script\Event;
-use Symfony\Component\Cache\Adapter\PhpFilesAdapter;
+use Symfony\Bundle\FrameworkBundle\DependencyInjection\FrameworkExtension;
 use Symfony\Component\Cache\Exception\CacheException;
+use Symfony\Component\Config\ConfigCache;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\Controller\ErrorController;
 
 /**
- * Retrieves the container object.
+ * Retrieves the configured container object. In production, the container
+ * is cached and stored at the path defined by kernel.cache_dir.
+ * ContainerFactory::dumpAutoload should be set as a composer script.
+ *
+ * ```
+ * "post-autoload-dump": [
+ *   "NewsHour\\WPCoreThemeComponents\\Containers\\ContainerFactory::dumpAutoload"
+ * ]
+ * ```
+ *
+ * @final
  */
 final class ContainerFactory
 {
-    public const CACHE_NAMESPACE = 'container';
-
     /**
      * @var Container
      */
@@ -38,17 +49,71 @@ final class ContainerFactory
         }
 
         try {
-            $containerBuilder = new ContainerBuilder();
-            $loader = new PhpFileLoader($containerBuilder, new FileLocator(__DIR__));
-            $loader->load('Configuration.php');
+            if (!defined('BASE_DIR')) {
+                trigger_error('The constant BASE_DIR (project root) must be defined.');
+            }
 
-            // Apply any container filters.
-            $containerBuilder = apply_filters('core_theme_container', $containerBuilder);
+            $cacheDir = \trailingslashit(BASE_DIR) . 'cache';
 
-            // ...now compile the container.
-            $containerBuilder->compile();
+            if (!is_dir($cacheDir)) {
+                mkdir($cacheDir, 0755);
+            }
 
-            self::$instance = $containerBuilder;
+            if (!is_writable($cacheDir)) {
+                trigger_error($cacheDir . ' dir is not writable.');
+            }
+
+            $cacheFile = $cacheDir . '/container.php';
+            $containerConfigCache = new ConfigCache($cacheFile, WP_DEBUG);
+
+            if (!$containerConfigCache->isFresh()) {
+
+                // Setup the container.
+                $containerBuilder = new ContainerBuilder();
+
+                // Add the framework bundle.
+                $frameworkExt = new FrameworkExtension();
+                $containerBuilder->registerExtension($frameworkExt);
+                $containerBuilder->loadFromExtension($frameworkExt->getAlias(), [
+                    'translator' => ['enabled' => false]
+                ]);
+
+                // Load any custom configs for the theme.
+                $themeExt = new DependencyInjection\ThemeExtension();
+                $containerBuilder->registerExtension($themeExt);
+                $containerBuilder->loadFromExtension($themeExt->getAlias());
+
+                // Load Symfony package configs.
+                $packages = new YamlFileLoader($containerBuilder, new FileLocator(trailingslashit(BASE_DIR) . 'config/packages'));
+                $packages->import('*', 'yaml');
+
+                $containerBuilder->setParameter('kernel.debug', WP_DEBUG);
+                $containerBuilder->setParameter('kernel.charset', 'utf-8');
+                $containerBuilder->setParameter('kernel.project_dir', BASE_DIR);
+                $containerBuilder->setParameter('kernel.cache_dir', \trailingslashit(BASE_DIR) . 'cache');
+                $containerBuilder->setParameter('kernel.build_dir', \trailingslashit(BASE_DIR) . 'cache/build');
+                $containerBuilder->setParameter('kernel.container_class', ContainerBuilder::class);
+                $containerBuilder->setParameter('kernel.error_controller', ErrorController::class);
+                $containerBuilder->setParameter('kernel.bundles_metadata', []);
+                $containerBuilder->setParameter('kernel.runtime_environment', WP_ENV);
+                $containerBuilder->setParameter('kernel.default_locale', 'en_US');
+
+                // Apply any container filters.
+                $containerBuilder = apply_filters('core_theme_container', $containerBuilder);
+
+                // ...now compile the container.
+                $containerBuilder->compile();
+
+                $dumper = new PhpDumper($containerBuilder);
+                $containerConfigCache->write(
+                    $dumper->dump(['class' => 'CoreThemeCachedContainer']),
+                    $containerBuilder->getResources()
+                );
+            }
+
+            require_once $cacheFile;
+            $containerClass = '\\CoreThemeCachedContainer';
+            self::$instance = new $containerClass;
         } catch (InvalidArgumentException $iae) {
             trigger_error($iae->getMessage(), E_USER_ERROR);
         } catch (Exception $e) {
@@ -68,9 +133,11 @@ final class ContainerFactory
     {
         try {
             $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
-            $cacheDir = rtrim($vendorDir, '/') . '/newshour/cache/';
-            $cacheAdapter = new PhpFilesAdapter(self::CACHE_NAMESPACE, 0, $cacheDir);
-            $cacheAdapter->delete('container');
+            $cachedContainer = dirname($vendorDir) . '/cache/container.php';
+
+            if (file_exists($cachedContainer)) {
+                unlink($cachedContainer);
+            }
         } catch (InvalidArgumentException $iae) {
             trigger_error($iae->getMessage(), E_USER_ERROR);
         } catch (CacheException $ce) {
