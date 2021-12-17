efff810f629de7cf28554f7539674781969c1781
Stephan Rohde <srohde@newshour.org>
2021-12-17 12:06:44 -0500

    Mitigating WP's magic quotes.

--- src/Http/Factories/RequestFactory.php
+++ src/Http/Factories/RequestFactory.php
@@ -6,6 +6,7 @@
 
 namespace NewsHour\WPCoreThemeComponents\Http\Factories;
 
+use Symfony\Component\HttpFoundation\InputBag;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 
@@ -45,9 +46,28 @@ final class RequestFactory
     public static function getStack(): RequestStack
     {
         if (self::$stack == null) {
-            $request = Request::createFromGlobals();
+
+            // Fixes for Wordpress's auto-escaping mutations.
+            $request = new Request(
+                stripslashes_deep($_GET),
+                stripslashes_deep($_POST),
+                [],
+                stripslashes_deep($_COOKIE),
+                $_FILES,
+                stripslashes_deep($_SERVER)
+            );
+
+            // Borrowed from Request's `createFromGlobals` method.
+            if (str_starts_with($request->headers->get('CONTENT_TYPE', ''), 'application/x-www-form-urlencoded')
+                && \in_array(strtoupper($request->server->get('REQUEST_METHOD', 'GET')), ['PUT', 'DELETE', 'PATCH'])
+            ) {
+                parse_str($request->getContent(), $data);
+                $request->request = new InputBag($data);
+            }
+
             $request->setLocale(get_locale());
             $request->setDefaultLocale(get_locale());
+
             $stack = new RequestStack();
             $stack->push($request);
             self::$stack = $stack;
