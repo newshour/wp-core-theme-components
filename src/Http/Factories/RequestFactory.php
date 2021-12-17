1edecb1a65484a4cd44d21ec05724e6ed45ec29c
Stephan Rohde <srohde@newshour.org>
2021-12-17 12:21:11 -0500

    Make sure we have arrays.

--- src/Http/Factories/RequestFactory.php
+++ src/Http/Factories/RequestFactory.php
@@ -46,19 +46,24 @@ final class RequestFactory
     public static function getStack(): RequestStack
     {
         if (self::$stack == null) {
-
             // Fixes for Wordpress's auto-escaping mutations.
+            $_get = stripslashes_deep($_GET);
+            $_post = stripslashes_deep($_POST);
+            $_cookie = stripslashes_deep($_COOKIE);
+            $_server = stripslashes_deep($_SERVER);
+
             $request = new Request(
-                stripslashes_deep($_GET),
-                stripslashes_deep($_POST),
+                is_array($_get) ? $_get : [],
+                is_array($_post) ? $_post : [],
                 [],
-                stripslashes_deep($_COOKIE),
+                is_array($_cookie) ? $_cookie : [],
                 $_FILES,
-                stripslashes_deep($_SERVER)
+                is_array($_server) ? $_server : []
             );
 
             // Borrowed from Request's `createFromGlobals` method.
-            if (str_starts_with($request->headers->get('CONTENT_TYPE', ''), 'application/x-www-form-urlencoded')
+            if (
+                str_starts_with($request->headers->get('CONTENT_TYPE', ''), 'application/x-www-form-urlencoded')
                 && \in_array(strtoupper($request->server->get('REQUEST_METHOD', 'GET')), ['PUT', 'DELETE', 'PATCH'])
             ) {
                 parse_str($request->getContent(), $data);
