4ff075a85701facd5a2019d2a799df9f55a5ac2e
Stephan Rohde <srohde@newshour.org>
2022-07-08 14:35:28 -0400

    Check for existence of config file.

--- src/CoreThemeTestCase.php
+++ src/CoreThemeTestCase.php
@@ -16,7 +16,21 @@ class CoreThemeTestCase extends KernelTestCase
      */
     protected static function createKernel(array $options = [])
     {
-        include_once getcwd() . '/config/application.php';
+        $baseDir = getcwd();
+
+        if ($baseDir === false) {
+            error_log('Could not determine the current working directory.');
+            exit;
+        }
+
+        $appEnv = rtrim($baseDir, '/') . '/config/application.php';
+
+        if (!file_exists($appEnv)) {
+            error_log('Could not find config/application.php at the project root.');
+            exit;
+        }
+
+        include_once $appEnv;
 
         return CoreThemeKernel::create('test', WP_DEBUG);
     }
